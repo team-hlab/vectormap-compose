13178ebe71b96d807a9a04616823e0d63ff80cea
alen.heo <hubtwork@gmail.com>
2023-08-24 23:30:17 +0900

    init gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://devrepo.kakao.com/nexus/repository/kakaomap-releases/") }
+    }
+}
+
+include(":vectormap-compose")
+include(":sample-app")
+
+rootProject.name = "vectormap-compose"
