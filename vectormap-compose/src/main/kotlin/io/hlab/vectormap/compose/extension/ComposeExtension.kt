b7c0f987dbf7eaa9a20814bb3ac2bbdfe8031a2c
alen.heo <hubtwork@gmail.com>
2023-08-27 22:23:48 +0900

    add GetMap Extension for internal usage

--- vectormap-compose/src/main/kotlin/io/hlab/vectormap/compose/extension/ComposeExtension.kt
+++ vectormap-compose/src/main/kotlin/io/hlab/vectormap/compose/extension/ComposeExtension.kt
@@ -1,5 +1,12 @@
 package io.hlab.vectormap.compose.extension
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentComposer
+import com.kakao.vectormap.KakaoMap
+import io.hlab.vectormap.compose.internal.MapApplier
 
 internal val NoPadding = PaddingValues()
+
+@Composable
+internal fun getMap(): KakaoMap = (currentComposer.applier as MapApplier).map
