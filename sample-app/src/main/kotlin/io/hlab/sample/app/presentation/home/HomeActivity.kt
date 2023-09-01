aa3eda89a5239cf794ef2d6621b529f65268e90e
alen.heo <hubtwork@gmail.com>
2023-09-01 17:17:26 +0900

    add base home screen

--- sample-app/src/main/kotlin/io/hlab/sample/app/presentation/home/HomeActivity.kt
+++ sample-app/src/main/kotlin/io/hlab/sample/app/presentation/home/HomeActivity.kt
@@ -1,14 +1,21 @@
 package io.hlab.sample.app.presentation.home
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import io.hlab.sample.app.presentation.theme.SampleTheme
 
 class HomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
         setContent {
             SampleTheme {
                 HomeScreen()
--- sample-app/src/main/kotlin/io/hlab/sample/app/presentation/home/HomeScreen.kt
+++ sample-app/src/main/kotlin/io/hlab/sample/app/presentation/home/HomeScreen.kt
@@ -11,20 +11,23 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.minimumInteractiveComponentSize
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -33,20 +36,70 @@ import androidx.compose.ui.unit.sp
 import io.hlab.sample.app.R
 import io.hlab.sample.app.presentation.component.HSpacer
 import io.hlab.sample.app.presentation.component.VSpacer
+import io.hlab.sample.app.presentation.theme.SampleTheme
 import io.hlab.vectormap.compose.KakaoMap
+import io.hlab.vectormap.compose.settings.MapDirection
+import io.hlab.vectormap.compose.settings.MapViewSettings
+import io.hlab.vectormap.compose.settings.MapWidgetPosition
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
+    val scaffoldState = rememberScaffoldState()
+
     Scaffold(
-        bottomBar = {
-            BottomBar()
-        },
+        modifier = modifier.fillMaxSize(),
+        scaffoldState = scaffoldState,
     ) { contentPadding ->
         MainContent(
-            modifier = Modifier.padding(contentPadding),
+            modifier = Modifier
+                .padding(contentPadding),
         )
+    }
+}
+
+@Composable
+private fun MainContent(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        MainKakaoMap(
+            modifier = Modifier.fillMaxSize(),
+        )
+        TopBar(
+            query = "구로구 구로동",
+            modifier = Modifier
+                .align(Alignment.TopCenter),
+        )
+        BottomBar(
+            modifier = Modifier
+                .align(Alignment.BottomCenter),
+        )
+    }
+}
+
+@Composable
+private fun TopBar(
+    query: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .statusBarsPadding()
+            .minimumInteractiveComponentSize()
+            .height(IntrinsicSize.Max)
+            .padding(top = 16.dp, start = 8.dp, end = 8.dp)
+            .background(color = SampleTheme.colors.surface),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        SearchingBar(
+            query = query,
+            modifier = Modifier.weight(weight = 1f, fill = true),
+        )
+        HSpacer(width = 8.dp)
+        NavigationButton()
     }
 }
 
@@ -55,11 +108,14 @@ private fun BottomBar(
     modifier: Modifier = Modifier,
 ) {
     Surface(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .background(color = SampleTheme.colors.surface)
+            .fillMaxWidth(),
         elevation = 8.dp,
     ) {
         Row(
             modifier = modifier
+                .navigationBarsPadding()
                 .fillMaxWidth()
                 .minimumInteractiveComponentSize(),
             verticalAlignment = Alignment.CenterVertically,
@@ -112,13 +168,14 @@ private fun SearchingBar(
             Icon(
                 modifier = Modifier.size(24.dp),
                 painter = painterResource(id = R.drawable.ic_h32_menu),
+                tint = SampleTheme.colors.onSurface,
                 contentDescription = "searchingBar_menu",
             )
             HSpacer(width = 8.dp)
             Text(
                 modifier = Modifier.weight(weight = 1f),
                 text = query,
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.4f),
+                color = SampleTheme.colors.onSurface.copy(alpha = 0.4f),
                 fontSize = 18.sp,
                 fontWeight = FontWeight.SemiBold,
             )
@@ -126,6 +183,7 @@ private fun SearchingBar(
             Icon(
                 modifier = Modifier.size(24.dp),
                 painter = painterResource(id = R.drawable.ic_h32_voice),
+                tint = SampleTheme.colors.onSurface,
                 contentDescription = "searchingBar_menu",
             )
         }
@@ -145,7 +203,7 @@ private fun NavigationButton(
     ) {
         Column(
             modifier = Modifier
-                .background(color = Color(0xff3396FF)),
+                .background(color = SampleTheme.customColors.naviColor),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
@@ -153,12 +211,12 @@ private fun NavigationButton(
                 modifier = Modifier.size(18.dp),
                 painter = painterResource(id = R.drawable.ic_h32_navigation),
                 contentDescription = "navigation",
-                tint = Color.White,
+                tint = SampleTheme.colors.surface,
             )
             VSpacer(height = 2.dp)
             Text(
                 text = "길찾기",
-                color = Color.White,
+                color = SampleTheme.colors.surface,
                 fontSize = 10.sp,
             )
         }
@@ -166,38 +224,22 @@ private fun NavigationButton(
 }
 
 @Composable
-private fun TopBar(
-    query: String,
+private fun MainKakaoMap(
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        modifier = modifier
-            .minimumInteractiveComponentSize()
-            .height(IntrinsicSize.Max),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center,
-    ) {
-        SearchingBar(
-            query = query,
-            modifier = Modifier.weight(weight = 1f, fill = true),
-        )
-        HSpacer(width = 8.dp)
-        NavigationButton()
-    }
-}
+    val density = LocalDensity.current
 
-@Composable
-private fun MainContent(
-    modifier: Modifier = Modifier,
-) {
-    Box(modifier = modifier) {
-        KakaoMap(
-            modifier = Modifier.fillMaxSize(),
-        )
-        TopBar(
-            query = "구로구 구로동",
-            modifier = Modifier
-                .padding(top = 16.dp, start = 8.dp, end = 8.dp),
+    val mapViewSettings = remember {
+        MapViewSettings(
+            compassPosition = MapWidgetPosition(
+                mapDirection = MapDirection.TopRight,
+                xPx = with(density) { 8.dp.toPx() },
+                yPx = with(density) { 100.dp.toPx() },
+            ),
         )
     }
+    KakaoMap(
+        modifier = modifier,
+        mapViewSettings = mapViewSettings,
+    )
 }
