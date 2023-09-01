92e7df0c2e12ea92fe3c76799b7a7ca172c60ae8
alen.heo <hubtwork@gmail.com>
2023-09-01 17:16:00 +0900

    mod sample theme with custom values

---
+++ sample-app/src/main/kotlin/io/hlab/sample/app/presentation/theme/RawColors.kt
@@ -0,0 +1,10 @@
+package io.hlab.sample.app.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+object RawColors {
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+
+    val SkyBlue = Color(0xff3396FF)
+}
